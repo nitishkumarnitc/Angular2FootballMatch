99e5730b7fa9cd854a565a70e0e274f31f7f0559
Sanjay Kumar Singh <Angular@2>
2017-02-07 18:26:43 +0530

    API token changes.

--- src/app/foot-ball.service.ts
+++ src/app/foot-ball.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import {Competition} from "./competition";
-import {Http, Response, Headers} from "@angular/http";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import {Observable} from "rxjs";
 import 'rxjs/Rx';
 
 @Injectable()
 export class FootBallService {
 
-  private getCompetitionsUrl="https://www.api.football-data.org/v1/competitions/";
+  private getCompetitionsUrl="http://www.api.football-data.org/v1/competitions/";
 
   constructor(private _http:Http) { }
 
   getCompetitions():Observable<Competition[]>{
 
-    let headers = new Headers();
-    headers.append('Content-Type',
-      'application/json');
-    // headers.append('Access-Control-Allow-Origin', '*');
-    //
+ 
+    let headers = new Headers({ 'Accept': 'application/json' });
+    let token = 'b705e8d107bc46c3990ba4fbf4f0f345';
+    headers.append('X-Auth-Token', token);
+    let options = new RequestOptions({ headers: headers });
 
-    return this._http.get(this.getCompetitionsUrl)
+    return this._http.get(this.getCompetitionsUrl, options)
       .map((response:Response)=> <Competition[]> response.json())
       .do(data=>console.log(JSON.stringify(data)));
   }
